c69f97a933f55e8e538f4a7aca311504bbd55edd
thomhines <thomhines@gmail.com>
2013-11-22 15:44:52 -0600

    Added test page to verify that all file types and imports are working properly

---
+++ test/js/coffee.coffee
@@ -0,0 +1,7 @@
+noshow = "none"
+doshow = "block"
+
+`document.addEventListener('DOMContentLoaded',function(){
+	document.querySelector(".coffee.fail").style.display=noshow;
+	document.querySelector(".coffee.success").style.display=doshow;
+});`
---
+++ test/js/scripts.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded',function(){	
+	document.querySelector(".js.fail").style.display='none';
+	document.querySelector(".js.success").style.display='block';
+});
+
+
+
+$(function() {
+	
+	$('.jquery.success').show();
+	$('.jquery.fail').hide();
+	
+});
